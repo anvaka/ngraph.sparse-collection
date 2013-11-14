3211d29b5b1ca7c47d41c105e32fd3754cd9d1a2
anvaka <anvaka@gmail.com>
2013-11-14 01:16:42 -0800

    Added example and updated package file

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * This library is huge. Import concrete graph you are interested in, like so:
+ * ``` js
+ *   var mtxObject = require('./HB/plat1919'),
+ *       mtxParser = require('ngraph.serialization/mtx'),
+ *       graaph mtxParser.loadFromObject(mtxObject);
+ * ```
+ */
+
